978896e678c56b417e05201bff6cecedaffe9893
Juer Genie Wand <2695996944@qq.com>
2017-09-28 00:36:17 +0800

    此类中包含了序列化与反序列化的方法，用于进行 [对象--Xml] 的转换，方便对象传输。

--- Drageon.Common/XmlUtils.cs
+++ Drageon.Common/XmlUtils.cs
@@ -8,7 +8,7 @@ namespace Drageon.Common
         public static T Deserialize<T>(string xml)
         {
             //初始化 XmlSerializer 时会报错，但并不影响运行——引发的异常:“System.IO.FileNotFoundException”(位于 mscorlib.dll 中)
-            //我不清楚是为什么，如果您知道的话，希望能够为我解惑，谢谢(:3)。
+            //我不清楚是为什么，如果您知道的话，希望能够为我解惑，谢谢(:3)，联系方式就在Readme中。
             XmlSerializer serializer = new XmlSerializer(typeof(T));
             StringReader reader = new StringReader(xml);
             T temp = (T)serializer.Deserialize(reader);
